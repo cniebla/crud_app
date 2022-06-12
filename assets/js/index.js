bd2dcd77318a1acce756036079a1b182223689c4
Carlos A. Niebla Becerra <cniebla@amd64.niebla>
2022-06-12 12:55:36 -0500

    start commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,4 @@
 
-
-
 $("#add_user").submit(function(event){
     alert("Data Inserted Successfully!");
 })
@@ -17,7 +15,7 @@ $("#update_user").submit(function(event){
 
 
     var request = {
-        "url" : `http://localhost:3000/api/users/${data.id}`,
+        "url" : `http://localhost:4000/api/users/${data.id}`,
         "method" : "PUT",
         "data" : data
     }
@@ -34,7 +32,7 @@ if(window.location.pathname == "/"){
         var id = $(this).attr("data-id")
 
         var request = {
-            "url" : `http://localhost:3000/api/users/${id}`,
+            "url" : `http://localhost:4000/api/users/${id}`,
             "method" : "DELETE"
         }
 
